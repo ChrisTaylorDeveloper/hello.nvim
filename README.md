e979b5d962c77a08043fb4fae744e1a9a190ea6c
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-02-14 15:44:19 +0100

    Reasonable working point.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# hello.nvim
-A dummy Neovim plugin.
+# semi-colon.nvim
+
+Remaps semi-colon to colon.
---
+++ plugin/enter.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("n", ";", ":")
+vim.keymap.set("v", ";", ":")
+vim.keymap.set("n", ";;", ";")
